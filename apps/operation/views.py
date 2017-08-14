bb177d667019f4a507408f018566c23d032927cb
Lurance <alphahsl@foxmail.com>
2017-08-14 21:17:17 +0800

    bug fix

--- apps/operation/views.py
+++ apps/operation/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import JsonResponse, HttpResponseNotFound
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
