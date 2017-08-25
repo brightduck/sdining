b5a3101d3618f6fb75e4024b2b60ed4ab94cd43f
Lurance <alphahsl@foxmail.com>
2017-08-25 09:53:11 +0800

    Add auto wrapper function

--- sdining/settings.py
+++ sdining/settings.py
@@ -163,5 +163,5 @@ CRONJOBS = [
     ('50 10 * * *', 'cronjob.order_wrapper_push.wrapper'),
     ('0,10,20,30,40 11 * * *', 'cronjob.order_wrapper_push.wrapper'),
     ('50 15 * * *', 'cronjob.order_wrapper_push.wrapper'),
-    ('0,10,20,30,40 11 * * *', 'cronjob.order_wrapper_push.wrapper'),
+    ('0,10,20,30,40 16 * * *', 'cronjob.order_wrapper_push.wrapper'),
 ]
